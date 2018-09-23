bb72e89223dc43e173e0795cfbc577f5a07c253f
Alexey Shutov <kalandrill@gmail.com>
2018-09-23 17:24:33 +0300

    added prime number generator

--- Corman/PrimeGenerator.cs
+++ Corman/PrimeGenerator.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Algorithms.Corman
 {
     public static class PrimeGenerator
     {
-        public static int GetNextClosestPrime(int i)
+        public static int GetNextClosestPrime(int number)
         {
-            throw new NotImplementedException();
+            var sieve = GetPrimeEnumeratorEratosthenes(number * 2);
+
+            while (!sieve.MoveNext() && sieve.Current < number){}
+            
+            return sieve.Current;
+        }
+
+        public static IEnumerator<int> GetPrimeEnumeratorEratosthenes(int number)
+        {
+            var sieve = new bool[number + 1];
+
+            for (int i = 2; i <= number; i++)
+            {
+                if (sieve[i]) continue;
+
+                yield return i;
+
+                for (int j = i*i; j <= number; j+=i)
+                {
+                    sieve[j] = true;
+                }
+            }
         }
     }
 }

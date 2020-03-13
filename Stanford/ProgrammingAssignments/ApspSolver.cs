263aadbd3649c66b7678cd4d904471b4ab1c8a76
rrrzzz <kalandrill@gmail.com>
2020-03-13 22:37:03 +0300

    brought back links, coursera's have short expiry date

--- Stanford/ProgrammingAssignments/ApspSolver.cs
+++ Stanford/ProgrammingAssignments/ApspSolver.cs
@@ -22,10 +22,10 @@ namespace Algorithms.Stanford.ProgrammingAssignments
         //
         // Answer: -19
 
-        private const string Link1 = "https://d3c33hcgiwev3.cloudfront.net/_6ff856efca965e8774eb18584754fd65_g1.txt?Expires=1584057600&Signature=Ig6aHCazWLeXeHzAKuuLB5ugWPpWd5A66STZ6mMlH6oYjAQmqMMgA77wGUYlv-MUV~QLwA-Z~IQB7KqzlbA8K3p-XYYCip96cvPIsVMpZGeJCdX9pWzCwhhMm5h3s~AAo6zwqyYYB~QbayO2kAZAxTR-xZqSAQuG5i1Og~GafIc_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A";
-        private const string Link2 = "https://d3c33hcgiwev3.cloudfront.net/_6ff856efca965e8774eb18584754fd65_g2.txt?Expires=1584057600&Signature=B9F2CWthoqiC6iZ9-mGhITfuClhJi5rmyBTqJOUA8XgV68MfctphmRxGrCojnRbbu1wqF6jnnCghXuzgUasIkpEmflV5Qu8Dfzj1fc7K0FwXYlPPRuupkTD1XlT-AMjJk4jWTYvSmQgU8fiLsOeP7bF4wv3B~CN9DfgaW0dvyLc_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A";
-        private const string Link3 = "https://d3c33hcgiwev3.cloudfront.net/_6ff856efca965e8774eb18584754fd65_g3.txt?Expires=1584057600&Signature=Lh8r25G3gs0FTt0URG89FGmlD0aP1yZ--cUtu7znXQyrORhVTEnAjz3UptxEVK~2ERfn5GsHKSRWY9N-EuU5BjTNRfPcPigjlwnkKAUDl5wfh-MhFJEbT2Y7bQEHtAxPIJw2WgKvB5ad5yYdWhcwwLjoQNg7z5FTzBhEEp-CiM8_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A";
-        private const string Link4 = "https://d3c33hcgiwev3.cloudfront.net/_919f8ed0c52d8b5926aa7e3fdecc2d32_large.txt?Expires=1584057600&Signature=EpO4kNU74nTGw0laFhm3pD8IVMNVJinPFw~mmQePsPrf4NZpT~izgKIJUoLfs6tol-DZg~dx~nt29KGJNb0v2tbKdeWF6v0~EUPVzmr-T2fqbqpH2c2oFWjFH~~-AbniCOCxr5UvYF04zKvMUY3b9VcfvU60wLswPqd1Zi5rPEY_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A";
+        private const string Link1 = "https://lagunita.stanford.edu/assets/courseware/v1/c614f1fbb78bca7c98067a9038976ffb/asset-v1:Engineering+Algorithms2+SelfPaced+type@asset+block/g1.txt";
+        private const string Link2 = "https://lagunita.stanford.edu/assets/courseware/v1/617a8dccb5474bdf76f973f553f83777/asset-v1:Engineering+Algorithms2+SelfPaced+type@asset+block/g2.txt";
+        private const string Link3 = "https://lagunita.stanford.edu/assets/courseware/v1/9f0236832060a0f2ccfcb43f07301b7d/asset-v1:Engineering+Algorithms2+SelfPaced+type@asset+block/g3.txt";
+        private const string Link4 = "https://s3-us-west-1.amazonaws.com/prod-edx/Algo2/Files/large.txt";
 
         public IEnumerable<int?> SolveFirstLink()
         {
@@ -36,8 +36,7 @@ namespace Algorithms.Stanford.ProgrammingAssignments
         public int SolveSecondLink()
         {
             var g = ParseWeightedGraphFromWeb(Link2);
-            var reweightedGraph = new JohnsonReweighting().ReweightEdges(g); 
-            return new DijkstraShortestPath().GetMinAllPairsShortestPaths(reweightedGraph);
+            return new DijkstraShortestPath().GetMinAllPairsShortestPaths(g);
         }
         
         public int SolveThirdLink()
